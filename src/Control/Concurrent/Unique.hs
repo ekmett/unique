3ccf7aef54f9b7d4825006bff2d3c380ea1e5c4a
Edward Kmett <ekmett@Edwards-MacBook.local>
2015-09-27 19:40:25 -0400

    repository initialized

---
+++ src/Control/Concurrent/Unique.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE UnboxedTuples #-}
+{-# LANGUAGE MagicHash #-}
+
+-- | An abstract interface to a concurrent unique symbol generator.
+--
+-- Unlike @Data.Unique@ from @base@ the values are not a member of 'Ord'. However, there is no global bottleneck.
+module Control.Concurrent.Unique
+  ( Unique, newUnique
+  ) where
+
+import Data.Hashable
+import GHC.IO
+import GHC.Prim
+import GHC.Types
+
+-- | Unique identifiers are created by creating heap objects in kind # that
+-- can be compared for value equality and then hashing them using their initial allocation
+-- address.
+
+-- TODO: If, due to a small heap size we find we have high collision rate on initial allocation location
+-- we might consider upgrading this initial hash with something fast and volatile, e.g. rdtsc
+data Unique = Unique !Int (MutableByteArray# RealWorld)
+
+instance Eq Unique where
+  Unique _ p == Unique _ q = isTrue# (sameMutableByteArray# p q)
+
+instance Hashable Unique where
+  hash (Unique i _) = i
+  hashWithSalt d (Unique i _) = hashWithSalt d i
+
+-- | Allocate a new 'Unique' value. The value returned will not compare equal to any other value of type 'Unique' returned by previous calls to 'newUnique'. There is no limit on the number of times 'newUnique' may be called.
+newUnique :: IO Unique
+newUnique = IO $ \s -> case newByteArray# 0# s of
+  (# s', ba #) -> (# s', Unique (I# (addr2Int# (unsafeCoerce# ba))) ba #)
